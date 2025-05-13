8fc37d2365a203d45e781b0114399ef67b330309
Pavel Gordienko <guzmo@yandex.ru>
2025-05-13 22:04:46 +0300

    fix: import  warning

--- billing/src/components/billing/ui/Billing.tsx
+++ billing/src/components/billing/ui/Billing.tsx
@@ -2,16 +2,18 @@ import { SectionHeader } from '@/components/sectionHeader';
 import { Reviews } from '@/components/reviews';
 import { Faq } from '@/components/faq';
 
-import {
-  billingHeader,
-  subHeader,
-  faqHeader,
-  supportText,
-  supportButtonText,
-} from '../model/data.json';
+import data from '../model/data.json';
 import { billingReviewsMock, faqMock } from '../__mocks__/billing.mock';
 
 export const Billing = () => {
+  const {
+    billingHeader,
+    subHeader,
+    faqHeader,
+    supportText,
+    supportButtonText,
+  } = data;
+
   return (
     <div className="mt-16 px-[104px] max-w-[1440px] mx-auto">
       <SectionHeader billingHeader={billingHeader} subHeader={subHeader} />
